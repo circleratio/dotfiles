4ff8d83606878ed69e886efe3f9f6cd92d2c355f
circleratio <tf@dsl.gr.jp>
2023-10-14 15:00:09 +0900

    [update] quote arguments

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
 function install_file () {
-    if [ ! -f $2 ]; then
-        echo Installing $1 $2
-        install -C $1 $2
-    elif [ $1 -nt $2 ]; then
-        echo Installing $1 $2
-        install -C $1 $2
+    if [ ! -f "$2" ]; then
+        echo Installing "$1" "$2"
+        install -C "$1" "$2"
+    elif [ "$1" -nt "$2" ]; then
+        echo Installing "$1" "$2"
+        install -C "$1" "$2"
     fi
 }
 
 function install_file_only_if_no_exist () {
-    if [ ! -f $2 ]; then
-        echo Installing $1 $2
-        install -C $1 $2
+    if [ ! -f "$2" ]; then
+        echo Installing "$1" "$2"
+        install -C "$1" "$2"
     fi
 }
 
@@ -25,7 +25,7 @@ done
 
 echo Install files...
 
-files=(.bash_profile .bashrc .screenrc bin/* .emacs.d/site-lisp/* .emacs.d/scripts/*)
+files=(.bash_profile .bashrc .bash_aliases .screenrc bin/* .emacs.d/site-lisp/* .emacs.d/scripts/*)
 
 for f in "${files[@]}"; do
     install_file "$f" ~/"$f"
