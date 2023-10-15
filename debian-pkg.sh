73373017cb15671ff93a778e5f74fa14c8764673
circleratio <tf@dsl.gr.jp>
2023-10-16 01:35:47 +0900

    initial commit

---
+++ debian-pkg.sh
@@ -0,0 +1,19 @@
+!/usr/bin/bash
+
+apt install \
+  git \
+  gpg \
+  vim emacs \
+  fzf \
+  python3 \
+  fetchmail procmail neomutt \
+  rsync \
+  texlive texlive-latex-extra texlive-fonts-extra texlive-extra-utils texlive-lang-japanese \
+  fonts-noto-cjk fonts-mplus \
+  fonts-ipafont-gothic fonts-ipafont-mincho \
+  fonts-ipaexfont-gothic fonts-ipaexfont-mincho \
+  fonts-ipamj-mincho \
+  fonts-vlgothic fonts-migmix \
+  imagemagick
+
+echo Done.
