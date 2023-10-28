45e4ebea79352246a6670ae53cf8b6f7b2713859
circleratio <tf@dsl.gr.jp>
2023-10-28 17:03:28 +0900

    [add] docker

--- debian-pkg.sh
+++ debian-pkg.sh
@@ -1,9 +1,12 @@
 !/usr/bin/bash
+SETUP_USER=tf
+
+apt update
 
 apt install \
+  lsb-release \
   git \
-  screen \n
-  gpg \
+  screen \
   vim emacs \
   fzf bat ripgrep \
   python3 \
@@ -16,6 +19,7 @@ apt install \
 
 # network
 apt install \
+  gnupg ca-certificates \
   curl wget w3m \
   fetchmail procmail neomutt \
   rsync
@@ -32,5 +36,15 @@ apt install \
 # jokes
 apt install \
   sysvbanner cmatrix
+
+# Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+usermod -aG docker ${SETUP_USER}
 
 echo Done.
