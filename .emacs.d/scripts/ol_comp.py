dfa9029bf4579b7f84185cf304791f52b0dc7b2c
circleratio <tf@dsl.gr.jp>
2024-11-16 20:01:57 +0900

    [add] send a mail using M365 Outlook.

---
+++ .emacs.d/scripts/ol_comp.py
@@ -0,0 +1,113 @@
+import win32com.client
+import sys
+import argparse
+import re
+import os
+
+class mail_draft:
+    def __init__(self):
+        self.mailfrom = ''
+        self.mail_to = ''
+        self.cc = ''
+        self.bcc = ''
+        self.subject = ''
+        self.body = ''
+        self.attachments = []
+
+    def print(self):
+        print('From: ' + self.mailfrom)
+        print('To: ' + self.mailto)
+        print('Cc: ' + self.cc)
+        print('Bcc: ' + self.bcc)
+        print('Subject: ' + self.subject)
+        print('<Body>\n' + self.body)
+        for a in self.attachments:
+            print(a)
+
+def parse_file(path):
+    md = mail_draft()
+    
+    with open(path, encoding='utf-8') as f:
+        lines = f.readlines()
+
+        in_body = False
+        for l in lines:
+            l = l.rstrip('\n\r')
+
+            if in_body:
+                md.body = md.body + l + '\n'
+            else:
+                if l == '---':
+                    in_body = True
+                else:
+                    m = re.match('[Ff]rom: *(.*)', l)
+                    if m:
+                        md.mailfrom = m.group(1)
+                        
+                    m = re.match('[Tt]o: *(.*)', l)
+                    if m:
+                        md.mailto = m.group(1)
+                        
+                    m = re.match('[Cc]c: *(.*)', l)
+                    if m:
+                        md.cc = m.group(1)
+                        
+                    m = re.match('[Bb]cc: *(.*)', l)
+                    if m:
+                        md.bcc = m.group(1)
+                        
+                    m = re.match('[Ss]ubject: *(.*)', l)
+                    if m:
+                        md.subject = m.group(1)
+
+                    m = re.match('[Aa]ttachment: *(.*)', l)
+                    if m:
+                        a = m.group(1)
+                        if os.path.isfile(a):
+                            md.attachments.append(a)
+                        else:
+                            print(f'Warning: {a} does not exist.')
+                    
+    return md
+
+def compose_mail(md, is_sent):
+    outlook = win32com.client.Dispatch('outlook.application')
+    mapi = outlook.GetNamespace('MAPI')
+
+    mail = outlook.CreateItem(0)
+
+    if md.mailto != '':
+        mail.to = md.mailto
+        
+    if md.cc != '':
+        mail.cc = md.cc
+        
+    if md.bcc != '':
+        mail.cc = md.bcc
+        
+    if md.subject != '':
+        mail.subject = md.subject
+        
+    mail.body = md.body
+
+    for a in md.attachments:
+        mail.Attachments.Add(a)
+
+    if is_sent:
+        mail.Send()
+    else:
+        mail.Save()
+
+def main():
+    parser = argparse.ArgumentParser(description='Compose a mail on M365 Outlook.')
+    
+    parser.add_argument('filename', help='a text file for a mail draft.')
+    parser.add_argument('-s', '--send', action='store_true')
+
+    args = parser.parse_args()
+
+    md = parse_file(args.filename)
+    compose_mail(md, False)
+
+if __name__ == '__main__':
+    main()
---
+++ .emacs.d/site-lisp/mail-draft-mode.el
@@ -0,0 +1,58 @@
+;;; package --- Mail Draft Mode
+(require 'cl-lib)
+
+(defvar mail-draft-mode-map (make-sparse-keymap))
+
+(define-key mail-draft-mode-map "\C-c\C-c" 'mail-draft-send)
+
+(defvar mail-draft-folder-root (concat (expand-file-name "~") "/Mail"))
+(defvar mail-draft-folder-drafts (concat mail-draft-folder-root "/drafts"))
+(defvar mail-draft-folder-sent (concat mail-draft-folder-root "/sent"))
+
+(defvar mail-draft-template "To: \nCc: \nSubject: \n---\n")
+
+(defvar mail-sender-script (concat (expand-file-name "~") "/.emacs.d/scripts/ol_comp.py"))
+
+(defun mail-draft-get-draft-filename()
+  (concat
+   mail-draft-folder-drafts
+   "/"
+   (number-to-string
+    (+ 1
+       (apply 'max
+              (cl-loop for elm in (cl-remove-if-not (lambda(s) (string-match "[0-9]+.txt" s))
+                                              (cons "0.txt" (directory-files mail-draft-folder-drafts)))
+                    collect (string-to-number (replace-regexp-in-string ".txt" "" elm))))))
+   ".txt"))
+
+(defun mail-draft-new ()
+  "Create a new mail draft and open it."
+  (interactive)
+  (find-file
+   (mail-draft-get-draft-filename))
+  (mail-draft-mode)
+  (insert mail-draft-template))
+
+(defun mail-draft-send ()
+  "Send the draft."
+  (interactive)
+  (save-buffer)
+  (let ((result (call-process "python" nil nil nil mail-sender-script (buffer-file-name))))
+    (cond ((eq result 0)
+           (message (concat (buffer-file-name) " was successfully sent."))
+           (kill-buffer))
+          (t (message "Failed to send the draft.")))))
+
+(defun mail-draft-mode ()
+  "A major mode for making a mail draft."
+  (interactive)
+  (kill-all-local-variables)
+  (setq mode-name "Mail Draft")
+  (setq major-mode 'mail-draft-mode)
+
+  (use-local-map mail-draft-mode-map)
+  (run-hooks 'mail-draft-mode-hook))
+
+(provide 'mail-draft-mode)
+  
+;;; mail-draft-mode.el ends here
