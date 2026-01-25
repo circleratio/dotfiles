220923b0b6fd5890100a3938861f80cce0107c6c
circleratio <tf@dsl.gr.jp>
2026-01-25 22:02:48 +0900

    [change] updated env

--- debian-pkg.sh
+++ debian-pkg.sh
@@ -9,7 +9,8 @@ apt install \
   vim emacs \
   fzf bat ripgrep \
   zoxide \
-  ncal
+  ncal \
+  qalc
 
 # development
 apt install \
@@ -20,12 +21,14 @@ apt install \
   python3 \
   python3-bottle \
   python3-bs4 \
+  python3-chardet \
+  python3-dateutil \
   python3-feedparser \
   python3-isort \
   python3-jinja2 \
   python3-numpy \
   python3-reportlab \
-  python3-dateutil \
+  python3-requests \
   python3-venv
 
 # media
@@ -55,16 +58,13 @@ apt install \
   sysvbanner cmatrix
 
 # Docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# echo \
+# "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+# $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt update
+# apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-usermod -aG docker ${SETUP_USER}
-
-# python modules
-pip install python-dateutil
+# usermod -aG docker ${SETUP_USER}
 
 echo Done.
